5ad88185071e0e4ccd1b7f230cd32cec0777d135
Dev Kumar <devkr074@gmail.com>
2024-12-01 10:48:51 +0530

    Initial Commit

--- JS/script.js
+++ JS/script.js
@@ -1,34 +1,51 @@
-const main = document.getElementById('main');
-const nav = document.getElementById('nav');
-const back = document.getElementById('back');
-const searchBtn = document.getElementById('search-btn');
-const searchBox = document.getElementById('search-box');
-const closeBtn = document.getElementById('close-btn');
-const openBtn = document.getElementById('open');
-const content2 = document.getElementById('content-2');
-main.addEventListener("scroll", function () {
-    if (main.scrollTop >= 120) {
-        console.log(main.scrollTop);
-        nav.classList.add("active");
-        back.classList.add('active');
+const navbar = document.querySelector("[data-navbar]");
+const navbarLinks = document.querySelectorAll("[data-nav-link]");
+const menuToggleBtn = document.querySelector("[data-menu-toggle-btn]");
+menuToggleBtn.addEventListener("click", function () {
+  navbar.classList.toggle("active");
+  this.classList.toggle("active");
+});
+for (let i = 0; i < navbarLinks.length; i++) {
+  navbarLinks[i].addEventListener("click", function () {
+    navbar.classList.toggle("active");
+    menuToggleBtn.classList.toggle("active");
+  });
+}
+const header = document.querySelector("[data-header]");
+const backTopBtn = document.querySelector("[data-back-top-btn]");
+window.addEventListener("scroll", function () {
+  if (window.scrollY >= 100) {
+    header.classList.add("active");
+    backTopBtn.classList.add("active");
+  } else {
+    header.classList.remove("active");
+    backTopBtn.classList.remove("active");
+  }
+});
+const searchBtn = document.querySelector("[data-search-btn]");
+const searchContainer = document.querySelector("[data-search-container]");
+const searchSubmitBtn = document.querySelector("[data-search-submit-btn]");
+const searchCloseBtn = document.querySelector("[data-search-close-btn]");
+const searchBoxElems = [searchBtn, searchSubmitBtn, searchCloseBtn];
+for (let i = 0; i < searchBoxElems.length; i++) {
+  searchBoxElems[i].addEventListener("click", function () {
+    searchContainer.classList.toggle("active");
+    document.body.classList.toggle("active");
+  });
+}
+const deliveryBoy = document.querySelector("[data-delivery-boy]");
+let deliveryBoyMove = -80;
+let lastScrollPos = 0;
+window.addEventListener("scroll", function () {
+  let deliveryBoyTopPos = deliveryBoy.getBoundingClientRect().top;
+  if (deliveryBoyTopPos < 500 && deliveryBoyTopPos > -250) {
+    let activeScrollPos = window.scrollY;
+    if (lastScrollPos < activeScrollPos) {
+      deliveryBoyMove += 1;
     } else {
-        nav.style.transform = `translateY(${-main.scrollTop}px)`;
-        nav.classList.remove("active");
-        back.classList.remove('active');
+      deliveryBoyMove -= 1;
     }
+    lastScrollPos = activeScrollPos;
+    deliveryBoy.style.transform = `translateX(${deliveryBoyMove}px)`;
+  }
 });
-back.addEventListener('click', function () {
-    main.scrollTop = 0;
-});
-searchBtn.addEventListener('click', function () {
-    searchBox.classList.toggle('active');
-    main.classList.toggle('active');
-});
-closeBtn.addEventListener('click', function () {
-    searchBox.classList.toggle('active');
-    main.classList.toggle('active');
-});
-openBtn.addEventListener('click',function(){
-    this.classList.toggle('active')
-    content2.classList.toggle('active');
-})
